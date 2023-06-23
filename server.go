431b5a52a54ce071eaa280f05a75e154653ac744
Aon <66352312+AonBalouch@users.noreply.github.com>
2023-06-24 01:09:51 +0500

    Update server.go

--- server.go
+++ server.go
@@ -1,35 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "os/exec"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
 )
 
 func main() {
-    // Download dependencies using "go mod download" command
-    cmd := exec.Command("go", "mod", "download")
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
-
-    // Start the server
-    http.HandleFunc("/", handleRequest)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handleRequest)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-    // Run your main.go file using "go run main.go" command
-    cmd := exec.Command("go", "run", "main.go")
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
+	cmd := exec.Command("go", "mod", "download")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Fprint(w, "Go project is running on Vercel!")
+	cmd = exec.Command("go", "run", "main.go")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprint(w, "Go project is running on Vercel!")
+}
+
+// Handler is the exported function that Vercel will invoke
+func Handler(w http.ResponseWriter, r *http.Request) {
+	handleRequest(w, r)
 }
